b81dc7e9cd448ea7bf62c8a9289878e10633f4a8
Koushik Ram <117310312+Itskoushik@users.noreply.github.com>
2025-03-05 23:40:55 +0530

    Update Find_Minimum_in_Rotated_Sorted_Array.py

--- 153 Leetcode/Find_Minimum_in_Rotated_Sorted_Array.py
+++ 153 Leetcode/Find_Minimum_in_Rotated_Sorted_Array.py
@@ -3,21 +3,21 @@ class Solution:
         res = nums[0]  # Initialize result with the first element
         l, r = 0, len(nums) - 1  # Set left and right pointers
         
-        while l <= r:
-            # If the current segment is sorted, the leftmost element is the smallest
-            if nums[l] < nums[r]:
-                res = min(res, nums[1])  # **BUG: Should be nums[l] instead of nums[1]**
-                break
+        while l <= r:  # Binary search loop
+            if nums[l] < nums[r]:  
+                # If the current segment [l:r] is sorted, the smallest element is nums[l]
+                res = min(res, nums[l])
+                break  # No need to search further
 
-            m = (l + r) // 2  # Calculate middle index
-            res = min(res, nums[m])  # Update result with the middle element
-            
-            # Determine which side to search next
+            m = (l + r) // 2  # Find middle index
+            res = min(res, nums[m])  # Update result with nums[m]
+
             if nums[m] >= nums[l]:  
-                # Left half is sorted, so min must be in the right half
+                # If nums[m] is greater than nums[l], it means the left half is sorted.
+                # The minimum must be in the right half.
                 l = m + 1
             else:  
-                # Right half is unsorted, so min must be in the left half
+                # If nums[m] is smaller than nums[l], the minimum is in the left half.
                 r = m - 1
         
-        return res
+        return res  # Return the minimum found
