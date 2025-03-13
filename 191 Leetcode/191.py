b8194c5a963dad0cc9e5f58e23dd823d6fbbc75b
Koushik Ram <117310312+Itskoushik@users.noreply.github.com>
2025-03-13 16:48:48 +0530

    Update 191.py

--- 191 Leetcode/191.py
+++ 191 Leetcode/191.py
@@ -1,1 +1,10 @@
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        res = 0  # Initialize a counter to store the number of 1s in the binary representation
+        
+        while n:  # Loop runs until all bits in `n` become 0
+            n &= (n - 1)  # This operation removes the rightmost set bit (1) in `n`
+            res += 1  # Increment the counter for each set bit removed
+        
+        return res  # Return the total count of 1s in the binary representation
 
