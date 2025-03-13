e25ba4509ba6c0b5e440028c94f57b1e5cd41f3c
Koushik Ram <117310312+Itskoushik@users.noreply.github.com>
2025-03-13 18:43:09 +0530

    Update 268.py

--- 268 Leetcode/268.py
+++ 268 Leetcode/268.py
@@ -1,1 +1,8 @@
-
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        res = len(nums)  # Initialize `res` with `n` (the length of the list)
+        
+        for i in range(len(nums)):  # Iterate over the indices of the list
+            res += (i - nums[i])  # Adjust `res` by adding the difference between index and value
+        
+        return res  # The final value of `res` gives the missing number
